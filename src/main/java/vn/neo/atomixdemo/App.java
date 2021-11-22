49c76dc14b43bb6872110d44dc38e383181f0b5e
acer <thanglv@neo.vn>
2021-11-22 16:59:51 +0700

    xxx

--- src/main/java/vn/neo/atomixdemo/App.java
+++ src/main/java/vn/neo/atomixdemo/App.java
@@ -1,7 +1,5 @@
 package vn.neo.atomixdemo;
 
-import io.atomix.core.Atomix;
-import io.atomix.core.AtomixConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,13 +8,6 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                AtomixConfig atomixConfig = Atomix.config("D:\\Idea Projects\\atomix-demo\\src\\main\\resources\\atomix.conf");
-                Atomix.builder(atomixConfig).build().start().join();
-            }
-        }).start();
     }
 
 }
--- src/main/java/vn/neo/atomixdemo/config/AppConfig.java
+++ src/main/java/vn/neo/atomixdemo/config/AppConfig.java
@@ -1,8 +1,10 @@
 package vn.neo.atomixdemo.config;
 
-import io.atomix.core.Atomix;
-import io.atomix.core.AtomixBuilder;
-import io.atomix.core.AtomixConfig;
+import com.alipay.remoting.rpc.RpcServer;
+import com.alipay.sofa.jraft.RaftGroupService;
+import com.alipay.sofa.jraft.entity.PeerId;
+import com.alipay.sofa.jraft.option.NodeOptions;
+import com.alipay.sofa.jraft.rpc.RaftRpcServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,33 @@ public class AppConfig {
 
     public AppConfig(ClusterAutoConfiguration clusterConfig) {
         this.clusterConfig = clusterConfig;
+    }
+
+    @Bean
+    public RaftGroupService test() throws InterruptedException {
+        PeerId serverId = new PeerId();
+        serverId.parse("localhost:6788");
+        RpcServer rpcServer = new RpcServer(serverId.getPort());
+        RaftRpcServerFactory.addRaftRequestProcessors(rpcServer);
+        NodeOptions nodeOptions = new NodeOptions();
+        // 为了测试, 调整 snapshot 间隔等参数
+        nodeOptions.setElectionTimeoutMs(5000);
+        nodeOptions.setDisableCli(false);
+        nodeOptions.setSnapshotIntervalSecs(30);
+        com.alipay.sofa.jraft.conf.Configuration configuration = new com.alipay.sofa.jraft.conf.Configuration();
+        configuration.parse("localhost:6788,localhost:6789");
+        nodeOptions.setInitialConf(configuration);
+        // 设置存储路径
+        // 日志, 必须
+        nodeOptions.setLogUri("D:/temp/log");
+        // 元信息, 必须
+        nodeOptions.setRaftMetaUri("D:/temp/raft_meta");
+        // snapshot, 可选, 一般都推荐
+        nodeOptions.setSnapshotUri("D:/temp/snapshot");
+        // 初始化 raft group 服务框架
+        RaftGroupService aftGroupService = new RaftGroupService("abc", serverId, nodeOptions, rpcServer);
+        aftGroupService.start().join();
+        return aftGroupService;
     }
 
 }
