7865d562b6336b57d4e04d2e43c4adb451377f2a
acer <thanglv@neo.vn>
2021-11-22 14:28:08 +0700

    xxx

--- src/main/java/vn/neo/atomixdemo/App.java
+++ src/main/java/vn/neo/atomixdemo/App.java
@@ -1,15 +1,22 @@
 package vn.neo.atomixdemo;
 
+import io.atomix.core.Atomix;
+import io.atomix.core.AtomixConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import vn.neo.atomixdemo.config.AtomixServer;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        AtomixServer.getInstance().start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                AtomixConfig atomixConfig = Atomix.config("D:\\Idea Projects\\atomix-demo\\src\\main\\resources\\atomix.conf");
+                Atomix.builder(atomixConfig).build().start().join();
+            }
+        }).start();
     }
 
 }
