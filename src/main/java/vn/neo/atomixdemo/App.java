efd3acc6e4f290cbb65e4f9f4c412acf1a67f2bc
acer <thanglv@neo.vn>
2021-11-22 14:25:26 +0700

    xxx

--- src/main/java/vn/neo/atomixdemo/App.java
+++ src/main/java/vn/neo/atomixdemo/App.java
@@ -2,12 +2,14 @@ package vn.neo.atomixdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import vn.neo.atomixdemo.config.AtomixServer;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+        AtomixServer.getInstance().start();
     }
 
 }
--- src/main/java/vn/neo/atomixdemo/config/AppConfig.java
+++ src/main/java/vn/neo/atomixdemo/config/AppConfig.java
@@ -1,19 +1,10 @@
 package vn.neo.atomixdemo.config;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import io.atomix.cluster.AtomixCluster;
-import io.atomix.cluster.AtomixClusterBuilder;
-import io.atomix.cluster.Node;
-import io.atomix.cluster.discovery.BootstrapDiscoveryProvider;
 import io.atomix.core.Atomix;
 import io.atomix.core.AtomixBuilder;
-import io.atomix.core.profile.Profile;
-import io.atomix.utils.net.Address;
+import io.atomix.core.AtomixConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author acer on 11/20/2021
@@ -28,29 +19,4 @@ public class AppConfig {
         this.clusterConfig = clusterConfig;
     }
 
-    @Bean
-    public AtomixCluster atomix() throws JsonProcessingException {
-        List<Node> members = new ArrayList<>();
-        clusterConfig.getMembers().forEach(item -> {
-            members.add(Node.builder()
-                    .withId(item.getNodeId())
-                    .withAddress(new Address(item.getHost(), item.getPort()))
-                    .build()
-            );
-        });
-        AtomixClusterBuilder builder = AtomixCluster.builder();
-        AtomixCluster atomix = builder
-                .withClusterId(clusterConfig.getClusterId())
-                .withMemberId(clusterConfig.getNodeId())
-                .withHost(clusterConfig.getHost())
-                .withPort(clusterConfig.getPort())
-                .withMembershipProvider(
-                        BootstrapDiscoveryProvider.builder()
-                                .withNodes(members)
-                                .build()
-                )
-                .build();
-        atomix.start().join();
-        return atomix;
-    }
 }
